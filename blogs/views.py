627bbfb85501551408fc689330cde6cc451c2744
Sidharth <sidham@deloitte.com>
2019-12-12 19:04:10 +0530

    added home page

--- blogs/views.py
+++ blogs/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 from blogs.models import Blog
 
 # Create your views here.
+def index(request):
+    recent_blogs = Blog.objects.all().order_by('-date')[:15]
+    print(len(recent_blogs))
+    return render(request, 'blogs/home.html', {'recent_blogs': recent_blogs})
